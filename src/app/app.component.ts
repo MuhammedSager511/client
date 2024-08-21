94d19598f6976099a2c1eba437033198a65abc74
MuhammedSager511 <mhmdsaghr748@gmail.com>
2024-08-21 11:14:37 +0300

    client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  baseURL:string ="https://localhost:7002/api/";
+  Message:any;
+
+
+  constructor(private http:HttpClient){
+
+  }
+
+  ngOnInit(): void {
+      this.getMessage();
+  }
+
+
+
+
+  getMessage(){
+    return this.http.get(this.baseURL+'Messages').subscribe({
+      next:(res)=>{
+        this.Message=res;
+        console.log(res);
+      },
+       error:(err)=>{
+        console.error(err);
+       }
+    })
+  }
 }
